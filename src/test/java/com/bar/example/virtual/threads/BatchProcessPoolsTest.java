ad301dd4cdace382e54ddb609ee136446b7e8f8a
mbarriola <mbarriola@gmail.com>
2023-01-14 21:45:52 -0500

    Added new tests

--- src/test/java/com/bar/example/virtual/threads/BatchProcessPoolsTest.java
+++ src/test/java/com/bar/example/virtual/threads/BatchProcessPoolsTest.java
@@ -2,12 +2,16 @@ package com.bar.example.virtual.threads;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import com.bar.example.virtual.threads.BatchProcessPools.InputEntry;
 
 public class BatchProcessPoolsTest {
 
@@ -17,6 +21,7 @@ public class BatchProcessPoolsTest {
 	private double mean = 0.0;
 	private ExecutorService fixSizePoolService = Executors.newFixedThreadPool(8);
 	private ExecutorService cachedPoolService = Executors.newCachedThreadPool();
+	private List<InputEntry> inputEntries = new ArrayList<>();
 	
 	@Before
 	public void setup() {
@@ -30,6 +35,10 @@ public class BatchProcessPoolsTest {
 			sum = sum + arr[i];
 		}
 		mean =  sum / ARRAY_LENGTH;
+		for (int i = 0; i<1000; i++) {
+			
+			inputEntries.add(new InputEntry("https://pw1/section/sensors", "3747ke", "11:00,12/21/2022 9:00", "11:00,12/21/2022 12:00"));
+		}
 	}
 	
 	@Test
@@ -42,5 +51,17 @@ public class BatchProcessPoolsTest {
 	public void analyzeSensorDataCalculationCachedThreadPoolTest() throws InterruptedException, ExecutionException {
 		
 		assertEquals("Check array mean", mean, BatchProcessPools.analyzeSensorData(arr, cachedPoolService), DELTA);
+	}
+	
+	@Test
+	public void processDataFixedThreadPoolTest() throws InterruptedException, ExecutionException {
+		
+		BatchProcessPools.processData(inputEntries, fixSizePoolService);
+	}
+	
+	@Test
+	public void processDataCachedThreadPoolTest() throws InterruptedException, ExecutionException {
+		
+		BatchProcessPools.processData(inputEntries, cachedPoolService);
 	}
 }
